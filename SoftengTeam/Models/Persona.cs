ba15026c09a0c029bb096fb6d0f8736efc2b1665
GustavoNajera <tavinchi.com@gmail.com>
2017-03-29 12:08:12 -0600

    Se adapta la plantilla en estructura MVC con clase maestra

---
+++ SoftengTeam/Models/Persona.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SoftengTeam.Models
+{
+    public class Persona
+    {
+        public int codigo { get; set; }
+        public String nombre { get; set; }
+
+    }
+}
