ba9b7555b4ad07f913d1a6691fba86df7db13b3e
GustavoNajera <tavinchi.com@gmail.com>
2017-04-04 12:37:57 -0600

    Se inicia el proyecto

---
+++ SoftengTeam/Controllers/FacilidadesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SoftengTeam.Controllers
+{
+    public class FacilidadesController : Controller
+    {
+        // GET: Facilidades
+        public ActionResult Facilidades()
+        {
+            return View();
+        }
+    }
+}
---
+++ SoftengTeam/Controllers/OfertasController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SoftengTeam.Controllers
+{
+    public class OfertasController : Controller
+    {
+        // GET: Ofertas
+        public ActionResult Ofertas()
+        {
+            return View();
+        }
+    }
+}
